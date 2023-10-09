05a6934b324b541a1c46a327f38af37635e0b1ed
mahi10042000 <malinimahi25@gmail.com>
2023-10-09 09:11:25 +0530

    done

--- src/org/ddd/Samples.java
+++ src/org/ddd/Samples.java
@@ -7,6 +7,18 @@ public class Samples {
 		System.out.println(a);
 		
 		System.out.println("anu has done our work");
+		System.out.println("banu will complete the work");
+<<<<<< HEAD
+
+		System.out.println("banu will complete the work>>>>>");
+
+========tai
+
+       System.out.println("anu has done our work");
+
+>>>>>>>>> c8dleba30a25ffcca5ba81c4d91f502d18
+		
+		
 	}
 
 }
